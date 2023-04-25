3e9ea6fe022fe6342dac09cd0e599cd5b78f1cfa
Dharren09 <dharrydharrenzerah@gmail.com>
2023-04-25 19:08:33 +0300

    Lockboxes alogarithm

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Lockboxes"""
+
+def canUnlockAll(boxes):
+    unlocked = [False] * len(boxes)
+    unlocked[0] = True
+
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+
+        for key in boxes[box]:
+            if key < len(boxes) and not unlocked[key]:
+                unlocked[key] = True
+                stack.append(key)
+
+    return all(unlocked)
