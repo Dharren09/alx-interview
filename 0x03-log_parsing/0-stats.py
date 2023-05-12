d04fde355cddc073ded0ffc1b654a29e10e1075a
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-12 15:07:48 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,58 +8,40 @@ status code should printed in ascending order
 import re
 import sys
 
+# a regular xpression to match the input format
+regex = re.compile(
+    r'^(\d{1,3}\.\d{1,3}\.\d{1,3})\.\d{1,3} - \[(.*?)\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)$')
 
-def compute_metrics():
-    """defines a dictionary of keys for the possible HTTP status codes"""
-    status_dict = {"200": 0, "301": 0, "400": 0,
-                   "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-    total_file_size = 0
-    count = 0
-    try:
-        """uses a while loop to red stdin indefinately"""
-        while True:
-            line = sys.stdin.readline()
-            if not line:
-                break
+# Initialize variables
+total_size = 0
+status_codes = {}
 
-            st = r'\[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
-            pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - ' + st
-            
-            """each line is matched to a regular expression pattern"""
-            match = re.match(pattern, line)
-            """if line doesnt match pattern, script skips to next line"""
-            if not match:
-                continue
+# read lines in the stdin
+for line in sys.stdin:
+    # match the input format
+    match = regex.match(line)
+    if match:
+        # Get the ip addresses, date, status codes and file size
+        ip_address = match.group(1)
+        date = match.group(2)
+        status_code = int(match.group(3))
+        file_size = int(match.group(4))
 
-            status_code = match.group(3)
+        # Update the total size with increment
+        total_size += file_size
 
-            """if status code extracted is not an int. scripts skips to next"""
-            if not int(status_code):
-                continue
+        # Update the status codes dict
+        if status_code not in status_codes:
+            status_codes[status_code] = 0
+        status_codes[status_code] += 1
 
-            if status_code in status_dict:
-                count += 1
-                status_dict[status_code] += 1
-                total_file_size += int(match.group(4))
-            
-            """if count reaches 10, it prints total file size and count
-            in ascending order"""
-            if count == 10:
-                print("Total file size: {}".format(total_file_size))
-                sorted_dict = sorted(status_dict.items(),
-                                     key=lambda x: x)
-                for key, value in sorted_dict:
-                    if value != 0:
-                        print("{}: {}".format(key, value))
-                count = 0
-    except KeyboardInterrupt as e:
-        sorted_dict = sorted(status_dict.items(),
-                             key=lambda x: x)
-        for key, value in sorted_dict:
-            if value != 0:
-                print("{}: {}".format(key, value))
-        print(e)
+        # Print the statistics every 10 line
+        if status_code % 10 == 0:
+            print('File size: {}'.format(total_size))
+            for status_code in sorted(status_codes):
+                print('{}: {}'. format(status_code, status_codes[status_code]))
 
 
-if __name__ == "__main__":
-    compute_metrics()
+# Handle the keyboard interrupts
+if sys.stdin.isatty() and input('Press ENTER to continue...') == '':
+    sys.exit(0)
