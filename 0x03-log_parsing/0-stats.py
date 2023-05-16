d1095f30e2fd2685e2a2461325452a7438f13349
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-16 13:15:38 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,57 +1,49 @@
 #!/usr/bin/python3
-
-"""script that reads stdin line by line and computes the metrics"""
+"""scripts that reads stdin line by line and computes the metrics"""
 
 import sys
-import re
 
 
-def compute_metrics():
-    """reads stdin line by line and computes metrics"""
-    current_dict = {"200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0}
-
-    file_size = 0
-    count = 0
-    try:
-        while True:
-            line = sys.stdin.readline()
-            if not line:
-                break
-
-            kj = r'\[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
-            pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - ' + kj
-
-            match = re.match(pattern, line)
-            if not match:
-                continue
-
-            status_code = match.group(3)
-
-            if not int(status_code):
-                continue
-
-            if status_code in current_dict:
-                count += 1
-                current_dict[status_code] += 1
-                file_size += int(match.group(4))
-
-            if count == 10:
-                print("File size: {}".format(file_size))
-                sorted_dict = sorted(current_dict.items(),
-                                     key=lambda x: x)
-                for key, value in sorted_dict:
-                    if value != 0:
-                        print("{}: {}".format(key, value))
-                count = 0
-    except KeyboardInterrupt as e:
-        sorted_dict = sorted(current_dict.items(),
-                             key=lambda x: x)
-        for key, value in sorted_dict:
-            if value != 0:
-                print("{}: {}".format(key, value))
-        print(e)
+def compute_metrics(file_size, status):
+    """Prints stats"""
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
 
 
 if __name__ == "__main__":
-    compute_metrics()
+    """Main function"""
+    status = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+    }
+    counter = 0
+    file_size = 0
+    try:
+        for line in sys.stdin:
+            if counter == 10:
+                compute_metrics(file_size, status)
+                counter = 1
+            else:
+                counter += 1
+            line = line.split()
+            try:
+                file_size += int(line[-1])
+            except Exception as e:
+                pass
+            try:
+                for key, value in status.items():
+                    if line[-2] == key:
+                        status[key] += 1
+            except Exception as e:
+                pass
+        compute_metrics(file_size, status)
+    except KeyboardInterrupt as e:
+        compute_metrics(file_size, status)
