c2859405e8096f3576f0416893b3b351fa091442
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-16 11:56:03 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,57 @@
 #!/usr/bin/python3
 
-"""
-reads stdin line by line and computes the metrics.
-status code should printed in ascending order
-"""
+"""script that reads stdin line by line and computes the metrics"""
 
-import re
 import sys
-
-# a regular xpression to match the input format
-regex = re.compile(
-    r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)$'
-)
-
-# Initialize variables
-total_size = 0
-status_codes = {}
-
-# read lines in the stdin
-for line in sys.stdin:
-    # match the input format
-    match = regex.match(line)
-    if match:
-        # Get the ip addresses, date, status codes and file size
-        ip_address = match.group(1)
-        date = match.group(2)
-        status_code = int(match.group(3))
-        file_size = int(match.group(4))
-
-        # Update the total size with increment
-        total_size += file_size
-
-        # Update the status codes dict
-        if status_code not in status_codes:
-            status_codes[status_code] = 0
-        status_codes[status_code] += 1
-
-        # Print the statistics every 10 line
-        if status_code % 10 == 0:
-            print('File size: {}'.format(total_size))
-            for status_code in sorted(status_codes):
-                print('{}: {}'. format(status_code, status_codes[status_code]))
+import re
 
 
-# Handle the keyboard interrupts
-if sys.stdin.isatty() and input('Press ENTER to continue...') == '':
-    sys.exit(0)
+def compute_metrics():
+    """reads stdin line by line and computes metrics"""
+    current_dict = {"200": 0, "301": 0, "400": 0, "401": 0,
+                    "403": 0, "404": 0, "405": 0, "500": 0}
+
+    file_size = 0
+    count = 0
+    try:
+        while True:
+            line = sys.stdin.readline()
+            if not line:
+                break
+
+            kj = r'\[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
+            pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - ' + kj
+
+            match = re.match(pattern, line)
+            if not match:
+                continue
+
+            status_code = match.group(3)
+
+            if not int(status_code):
+                continue
+
+            if status_code in current_dict:
+                count += 1
+                current_dict[status_code] += 1
+                file_size += int(match.group(4))
+
+            if count == 10:
+                print("File size: {}".format(file_size))
+                sorted_dict = sorted(current_dict.items(),
+                                     key=lambda x: x)
+                for key, value in sorted_dict:
+                    if value != 0:
+                        print("{}: {}".format(key, value))
+                count = 0
+    except KeyboardInterrupt as e:
+        sorted_dict = sorted(current_dict.items(),
+                             key=lambda x: x)
+        for key, value in sorted_dict:
+            if value != 0:
+                print("{}: {}".format(key, value))
+        print(e)
+
+
+if __name__ == "__main__":
+    compute_metrics()
