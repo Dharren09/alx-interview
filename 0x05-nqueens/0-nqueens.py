0ada355cfc4082337e3b1a5043e538fe17865db0
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-23 20:56:12 +0300

    N Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,39 +1,34 @@
 #!/usr/bin/python3
+
 import sys
+
 
 def is_safe(board, row, col):
     """Check if the current position is safe for the queen"""
     for i in range(col):
         """Check if there is a queen in the same row"""
-        if board[row][i] == 'Q':
+        if board[i] == row:
             return False
         """Check if there is a queen in the upper diagonal"""
-        if row - i - 1 >= 0 and board[row - i - 1][col - i - 1] == 'Q':
-            return False
-        """Check if there is a queen in the lower diagonal"""
-        if row + i + 1 < len(board) and board[row + i + 1][col - i - 1] == 'Q':
+        if board[i] == row - (col - i) or board[i] == row + (col - i):
             return False
     return True
 
-def solve_nqueens(board, col):
-    n = len(board)
+
+def solve_nqueens(board, col, n, solutions):
     if col >= n:
-        """Found a solution, print the board"""
-        print_board(board)
+        """Found a solution, add it to the list of solutions"""
+        solutions.append(list(board))
         return
     for row in range(n):
         if is_safe(board, row, col):
             """Place the queen at the current position"""
-            board[row][col] = 'Q'
+            board[col] = row
             """Recursively solve for the next column"""
-            solve_nqueens(board, col + 1)
+            solve_nqueens(board, col + 1, n, solutions)
             """Backtrack and remove the queen from the current position"""
-            board[row][col] = '.'
-            
-def print_board(board):
-    for row in board:
-        print(' '.join(row))
-    print()
+            board[col] = -1
+
 
 def nqueens(n):
     """Check if N is an integer"""
@@ -47,14 +42,17 @@ def nqueens(n):
         print("N must be at least 4")
         sys.exit(1)
     """Initialize the board"""
-    board = [['.' for _ in range(n)] for _ in range(n)]
-    """Solve the N Queens problem"""
-    solve_nqueens(board, 0)
+    board = [-1] * n
+    solutions = []
+    solve_nqueens(board, 0, n, solutions)
+    for solution in solutions:
+        formatted_solution = [[i, solution[i]] for i in range(n)]
+        print(formatted_solution)
+
 
 if __name__ == "__main__":
     """Check the number of arguments"""
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    """Solve the N Queens problem"""
     nqueens(sys.argv[1])
