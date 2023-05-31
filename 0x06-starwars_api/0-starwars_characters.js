56168455eda1746eeb12f8265ae64c9ed9e81530
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-31 15:37:36 +0300

    Starwars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+/* script that prinrs all characters of the star wars movies
+ * first postional arg passed is the movieID
+ * dipslays one character name per line in the ame order as
+ * charcters list
+ * uses stars wars api
+ * uses request module
+ */
+
+const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
+
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
